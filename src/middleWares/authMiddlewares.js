d1a0a48689d1303389d0fe5a89b552eebb8a3a64
you <nettanel1999@gmail.com>
2024-10-07 11:41:47 +0300

    middleWares empty

---
+++ src/middleWares/authMiddlewares.js
@@ -0,0 +1,20 @@
+const onlyCommanders = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const SoldiersAndCommanders = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = {
+    onlyCommanders,
+    SoldiersAndCommanders
+}
