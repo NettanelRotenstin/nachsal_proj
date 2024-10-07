6c362157e0ee303b65707478cc7b278d7d0c03e2
you <nettanel1999@gmail.com>
2024-10-07 10:31:33 +0300

    branch of router

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+const exp = require('express')
+const dotenv = require('dotenv')
+
+const app = exp()
+dotenv.config()
+const port = process.env.PORT || 1415
+
+app.use(exp.json())
+
+app.use('/auth',require('./routers/authRouter'))
+app.use('/user',require('./routers/userRoute'))
+app.use('/greenEye',require('./routers/greenEyeRouter'))
+
+app.listen(port,() => console.log(`server running on port ${port}`))
+
  
