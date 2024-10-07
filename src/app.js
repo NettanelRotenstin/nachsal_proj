76b61571b86701da51ae065a25f493144b1e338d
you <nettanel1999@gmail.com>
2024-10-07 10:56:11 +0300

    routers basic

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use('/auth',require('./routers/authRouter'))
 app.use('/user',require('./routers/userRoute'))
 app.use('/greenEye',require('./routers/greenEyeRouter'))
 
+
 app.listen(port,() => console.log(`server running on port ${port}`))
 
  
